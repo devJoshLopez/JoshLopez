3db348f2d3bc18a86d847aac320f5e4244129dce
Josh Lopez <devjoshlopez@gmail.com>
2019-02-04 00:11:30 -0700

    fix nav on desktop

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
   var staticWrapper = $('.static-wrapper');
   var tvToggleButton = $('#tv-toggle');
+  // var flyIn = $('.fly-in');
 
   var isTurnedOn = true;
 
@@ -46,5 +47,62 @@ $(document).ready(function () {
     tvToggleButton.toggleClass("off");
     toggleTV();
   });
+
+
+
+  // Set properties
+  TweenMax.set(".fly-in", {
+    perspective: 400
+  });
+  TweenMax.set(".fly-in div", {
+    rotationY: -10,
+    rotationX: 10,
+    rotation: 5,
+    scale: 0.2,
+    opacity: 0,
+    textShadow: "0px -6px 0 #212121, 0px -6px 0 #212121, 0px 6px 0 #212121, 0px 6px 0 #212121, -6px 0px 0 #212121, 6px 0px 0 #212121, -6px 0px 0 #212121, 6px 0px 0 #212121, -6px - 6px 0 #212121, 6px -6px 0 #212121, -6px 6px 0 #212121, 6px 6px 0 #212121, -6px 18px 0 #212121, 0px 18px 0 #212121, 6px 18px 0 #212121, 0 19px 1px #000000"
+  });
+
+  // Init timeline
+  var tl = new TimelineMax({
+    repeat: -1,
+    repeatDelay: 2
+  });
+
+  tl.to(".fly-in div:nth-child(1)", 1, {
+      scale: 1.1,
+      ease: Power4.easeIn
+    }, "firstDiv")
+    .to(".fly-in div:nth-child(1)", 1, {
+      opacity: 1,
+      ease: Power4.easeIn
+    }, "firstDiv")
+    .to(".fly-in div:nth-child(1)", 1, {
+      rotationY: 0,
+      rotationX: -7,
+      rotation: -10,
+      ease: Power4.easeIn
+    }, "firstDiv")
+    .to(".fly-in div:nth-child(1)", 1, {
+      opacity: 0,
+    }, "firstDiv+=3")
+
+    .to(".fly-in div:nth-child(2)", 1, {
+      scale: 1.1,
+      ease: Power4.easeIn
+    }, "secondDiv")
+    .to(".fly-in div:nth-child(2)", 1, {
+      opacity: 1,
+      ease: Power4.easeIn
+    }, "secondDiv")
+    .to(".fly-in div:nth-child(2)", 1, {
+      rotationY: 0,
+      rotationX: -7,
+      rotation: -15,
+      ease: Power4.easeIn
+    }, "secondDiv")
+    .to(".fly-in div:nth-child(2)", 1, {
+      opacity: 0,
+    }, "secondDiv+=3")
 
 });
