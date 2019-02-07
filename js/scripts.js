3b3152bd9b7bb84dd3911f2933bb7e1bcb4cb304
Josh Lopez <devjoshlopez@gmail.com>
2019-02-07 12:39:02 -0700

    fix pizza cursor

--- js/scripts.js
+++ js/scripts.js
@@ -110,123 +110,130 @@ $(document).ready(function () {
     }, "secondDiv+=3");
 
 
-    var turtlePower = $(".sewer-container");
-    var turtleTimeline = new TimelineMax();
+  var turtlePower = $(".sewer-container");
+  var turtleTimeline = new TimelineMax();
 
-    TweenMax.set(".sewer-cover", {
-      position: "absolute",
-      left: '50%',
+  TweenMax.set(".sewer-cover", {
+    position: "absolute",
+    left: '50%',
+    top: '50%',
+    xPercent: '-50',
+    yPercent: '-50',
+  });
+
+  turtleTimeline
+    .to(".sewer-cover", 1, {
+      scale: 1.03,
+      ease: Power4.easeInOut
+    })
+    .to(".sewer-cover", 1, {
+      left: '40%',
       top: '50%',
-      xPercent: '-50',
-      yPercent: '-50',
-    });
-
-    turtleTimeline
-    .to(".sewer-cover", 1, {scale: 1.03, ease:Power4.easeInOut})
-   .to(".sewer-cover", 1, {left: '40%', top: '50%', ease:Power4.easeInOut}, "-=0.7")
-   .reverse(); //start in reversed state
+      ease: Power4.easeInOut
+    }, "-=0.7")
+    .reverse(); //start in reversed state
   //  .timeScale(4) //speed it up for testing
 
-  turtlePower.hover(function(){
-  //set the reversed state equal to inverse of current reversed state (toggle direction)
-  turtleTimeline.reversed(!turtleTimeline.reversed());
-}); 
-
-
-var pizzaBite = false;
-
-function showPizzaCursor(e) {
-  $(".turtle-content .cursor").css({
-    top : (e.screenY - 150),
-    left: (e.screenX - 10),
-    opacity: 1
+  turtlePower.hover(function () {
+    //set the reversed state equal to inverse of current reversed state (toggle direction)
+    turtleTimeline.reversed(!turtleTimeline.reversed());
   });
-  $(".sewer-hole").on('click' , bitePizzaCursor);
-};
 
 
-function bitePizzaCursor(e) {
-  if (!pizzaBite) {
-    pizzaBite = true;
-    pizzaSound.play();
-    $(".full-slice").css({
-      display : "none"
+  var pizzaBite = false;
+
+  function showPizzaCursor(e) {
+    $(".turtle-content .cursor").css({
+      top: (e.clientY - 80),
+      left: (e.clientX - 20),
+      opacity: 1
     });
-  };
-};
+    $(".sewer-hole").on('click', bitePizzaCursor);
+  }
 
-function hidePizzaCursor(e) {
-  $(".turtle-content .cursor").css({
-    opacity: 0
+
+  function bitePizzaCursor(e) {
+    if (!pizzaBite) {
+      pizzaBite = true;
+      pizzaSound.play();
+      $(".full-slice").css({
+        display: "none"
+      });
+    }
+  }
+
+  function hidePizzaCursor(e) {
+    $(".turtle-content .cursor").css({
+      opacity: 0
+    });
+  }
+
+  $(".turtle-content").on('mousemove', showPizzaCursor);
+  $(".turtle-content").on('mouseout', hidePizzaCursor);
+
+
+  const pizzaSound = new Howl({
+    src: ['../sounds/pizza-time-turtles-in-time.mp3'],
+    volume: 0.6
   });
-};
-
-$(".turtle-content").on('mousemove' , showPizzaCursor);
-$(".turtle-content").on('mouseout' , hidePizzaCursor);
-
-
-const pizzaSound = new Howl({
-  src: ['../sounds/pizza-time-turtles-in-time.mp3'],
-  volume: 0.6
-});
 
 
 
-const lightning = new TimelineMax({
-  paused: true
-});
-lightning.add(startLightning());
+  const lightning = new TimelineMax({
+    paused: true
+  });
+  lightning.add(startLightning());
 
-function startLightning(){
-	const tl = new TimelineMax()
-  const bg = document.querySelector('.bg')
-	const streak = document.querySelectorAll('.lightning-streak')
-  const pathLength = document.querySelector('.lightning-streak__path').getTotalLength()
+  function startLightning() {
+    const tl = new TimelineMax();
+    const bg = document.querySelector('.bg');
+    const streak = document.querySelectorAll('.lightning-streak');
+    const pathLength = document.querySelector('.lightning-streak__path').getTotalLength();
 
-  tl.set(streak, {
-    opacity: 1,
-		strokeWidth: 0.5,
-    strokeDasharray: pathLength,
-    strokeDashoffset: pathLength,
-  })
-  tl.staggerTo(streak, 0.25, {
-    strokeDashoffset: 0,
-    stroke: 'white',
-    filter: 'drop-shadow( -1px -1px 1px #f9f175 )',
-		ease: Linear.easeIn
-	}, -0.1)
-  tl.staggerTo(streak, 0.4, {
-    opacity: 0,
-		ease: Bounce.easeOut
-	}, -0.02)
-  tl.to(streak, 0.1, {
-    opacity: 1,
-    strokeWidth: '2px',
-    filter: 'drop-shadow( -1px -1px 4px #6304c9 )',
-		ease: Linear.easeOut
-	})
-  tl.to(streak, 0.2, {
-    strokeWidth: '0.5px',
-    filter: 'drop-shadow( -1px -1px 4px #4e2db7 )',
-		ease: Linear.easeOut
-	})
-  tl.staggerTo(streak, 0.1, {
-    opacity: 0,
-		ease: Linear.easeOut
-	}, -0.1)
+    tl.set(streak, {
+      opacity: 1,
+      strokeWidth: 0.5,
+      strokeDasharray: pathLength,
+      strokeDashoffset: pathLength,
+    });
+    tl.staggerTo(streak, 0.25, {
+      strokeDashoffset: 0,
+      stroke: 'white',
+      filter: 'drop-shadow( -1px -1px 1px #f9f175 )',
+      ease: Linear.easeIn
+    }, -0.1);
+    tl.staggerTo(streak, 0.4, {
+      opacity: 0,
+      ease: Bounce.easeOut
+    }, -0.02);
+    tl.to(streak, 0.1, {
+      opacity: 1,
+      strokeWidth: '2px',
+      filter: 'drop-shadow( -1px -1px 4px #6304c9 )',
+      ease: Linear.easeOut
+    });
+    tl.to(streak, 0.2, {
+      strokeWidth: '0.5px',
+      filter: 'drop-shadow( -1px -1px 4px #4e2db7 )',
+      ease: Linear.easeOut
+    });
+    tl.staggerTo(streak, 0.1, {
+      opacity: 0,
+      ease: Linear.easeOut
+    }, -0.1);
 
-	return tl
-}
+    return tl;
+  }
 
-$("#lightning-button").on('click', function () {
-  lightning.restart();
-});
+  $("#lightning-button").on('click', function () {
+    lightning.restart();
+  });
 
-function randomLightning() {
-  lightning.restart();
-  setTimeout(randomLightning, Math.random() * 6000);
-}
-randomLightning();
+  function randomLightning() {
+    lightning.restart();
+    setTimeout(randomLightning, Math.random() * 6000);
+  }
+  randomLightning();
 
 
 });
