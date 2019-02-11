10ee3f389fdcf7de8f9975bee35347f71b01e7ec
DevJoshLopez <devjoshlopez@gmail.com>
2019-02-11 14:35:11 -0700

    add portfolio items

--- js/scripts.js
+++ js/scripts.js
@@ -314,4 +314,36 @@ $(document).ready(function () {
   });
 
 
+  // add glitch effect on img on hover and remove when not
+  $('.card-item').hover(
+    function () {
+      $(this).find('.card-item-image').addClass("glitch-img");
+
+      $(this).find('.card-item-image').mgGlitch({
+        destroy: false,
+        glitch: true,
+        scale: true,
+        blend: true,
+        blendModeType: 'hue',
+        glitch1TimeMin: 200,
+        glitch1TimeMax: 400,
+        glitch2TimeMin: 10,
+        glitch2TimeMax: 100,
+      });
+    },
+    function () {
+      $(this).find('.card-item-image').removeClass("glitch-img");
+      $(this).find('.card-item-image').mgGlitch({
+        destroy: true
+      });
+    }
+  );
+
+  // show cards when scrolling
+  ScrollReveal({
+    reset: true
+  }).reveal('.card-item');
+
+
+
 });
