eefb1925fdde61655f8ce8f5c1b00153dcf0b785
Josh Lopez <devjoshlopez@gmail.com>
2019-02-12 13:55:46 -0700

    url updates

--- js/scripts.js
+++ js/scripts.js
@@ -369,7 +369,7 @@ $(document).ready(function () {
 
 
   var scene = new THREE.Scene();
-  scene.fog = new THREE.FogExp2(0x111111, .13); // distance background color
+  scene.fog = new THREE.FogExp2(0x111111, 0.13); // distance background color
   var hackersWrapper = $('.hackers-wrapper');
 
   var camera = new THREE.PerspectiveCamera(42, hackersWrapper.innerWidth() / hackersWrapper.innerHeight(), 0.1, 2000);
@@ -391,7 +391,7 @@ $(document).ready(function () {
   }
 
   //Floor
-  var floorG = new THREE.BoxGeometry(20, .10, 20);
+  var floorG = new THREE.BoxGeometry(20, 0.10, 20);
   var floorM = new THREE.MeshLambertMaterial({
     color: 0x111111
   });
@@ -401,19 +401,19 @@ $(document).ready(function () {
   //Buildings
   var cube = [];
   for (var i = 0; i < 800; ++i) {
-    var rHeight = (Math.random() * 5) + .25;
-    var geometry = new THREE.BoxGeometry(.25, rHeight, .25);
+    var rHeight = (Math.random() * 5) + 0.25;
+    var geometry = new THREE.BoxGeometry(0.25, rHeight, 0.25);
     var material = new THREE.MeshLambertMaterial({
       color: 0x65eee9
     });
     material.transparent = true;
-    material.opacity = .2;
+    material.opacity = 0.2;
     cube[i] = new THREE.Mesh(geometry, material);
     floor.add(cube[i]);
 
-    var x = (Math.random() * (10.00 - -10) + -10).toFixed(2);
+    var x = (Math.random() * (10.00 - (-10)) + -10).toFixed(2);
     var y = 0;
-    var z = (Math.random() * (10.00 - -10) + -10).toFixed(2);
+    var z = (Math.random() * (10.00 - (-10)) + -10).toFixed(2);
     cube[i].position.set(x, y, z);
   }
 
@@ -425,15 +425,15 @@ $(document).ready(function () {
   scene.add(light1);
   light1.position.set(1.5, 2, 1);
 
-  var light1 = new THREE.DirectionalLight(0xffffff, .5);
-  scene.add(light1);
-  light1.position.set(-1.5, 2, 1);
+  var light2 = new THREE.DirectionalLight(0xffffff, 0.5);
+  scene.add(light2);
+  light2.position.set(-1.5, 2, 1);
 
 
   var distance = 0;
   var floorRotation = 1;
   var cameraPosition = 1;
-  var easingAmount = .001;
+  var easingAmount = 0.001;
 
   function render() {
     requestAnimationFrame(render);
