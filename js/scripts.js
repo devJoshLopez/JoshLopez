a45db09f4a2b6371f1c148ff1243cc7f77e264e6
Josh Lopez <devjoshlopez@gmail.com>
2019-02-11 18:21:24 -0700

    add tilt to portfolio and gibson

--- js/scripts.js
+++ js/scripts.js
@@ -271,33 +271,33 @@ $(document).ready(function () {
 
   function showDelorean(e) {
     deloreanTimeline
-    .to(".delorean", 1, {
-      left: '50%',
-      ease: Power4.easeInOut
-    });
+      .to(".delorean", 1, {
+        left: '50%',
+        ease: Power4.easeInOut
+      });
   }
 
   function hideDelorean(e) {
     deloreanTimeline
-    .to(".delorean", 1, {
-      left: '-150%',
-      ease: Power4.easeInOut
-    })
+      .to(".delorean", 1, {
+        left: '-150%',
+        ease: Power4.easeInOut
+      })
 
-    .to(".fire", 1, {
-      opacity: '1',
-      ease: Back.easeOut.config(1.4)
-    }, "-=0.65")
+      .to(".fire", 1, {
+        opacity: '1',
+        ease: Back.easeOut.config(1.4)
+      }, "-=0.65")
 
-    .set(".delorean", {
-      position: "absolute",
-      left: '150%'
-    })
+      .set(".delorean", {
+        position: "absolute",
+        left: '150%'
+      })
 
-    .to(".fire", 1, {
-      opacity: '0',
-      ease: Power4.easeInOut
-    }, "+=0.9");
+      .to(".fire", 1, {
+        opacity: '0',
+        ease: Power4.easeInOut
+      }, "+=0.9");
   }
 
   $(".bttf-content").on('mouseenter', showDelorean);
@@ -306,10 +306,25 @@ $(document).ready(function () {
   $("#lightning-button").on('click', function () {
     $(".more-content").toggleClass("show");
     var duration = 0.3,
-        delay = 0.08;
-    TweenMax.to("#lightning-button", duration, {scaleY: 1.6, ease: Expo.easeOut});
-    TweenMax.to("#lightning-button", duration, {scaleX: 1.2, scaleY: 1, ease: Back.easeOut, easeParams: [3], delay: delay});
-    TweenMax.to("#lightning-button", duration * 1.25, {scaleX: 1, scaleY: 1, ease: Back.easeOut, easeParams: [6], delay: delay * 3 });
+      delay = 0.08;
+    TweenMax.to("#lightning-button", duration, {
+      scaleY: 1.6,
+      ease: Expo.easeOut
+    });
+    TweenMax.to("#lightning-button", duration, {
+      scaleX: 1.2,
+      scaleY: 1,
+      ease: Back.easeOut,
+      easeParams: [3],
+      delay: delay
+    });
+    TweenMax.to("#lightning-button", duration * 1.25, {
+      scaleX: 1,
+      scaleY: 1,
+      ease: Back.easeOut,
+      easeParams: [6],
+      delay: delay * 3
+    });
 
   });
 
@@ -339,10 +354,116 @@ $(document).ready(function () {
     }
   );
 
-  // show cards when scrolling
+  // show cards and sections when scrolling
   ScrollReveal({
     reset: true
   }).reveal('.card-item');
+
+  // ScrollReveal({
+  //   reset: true
+  // }).reveal('section');
+
+
+
+
+
+
+  var scene = new THREE.Scene();
+  scene.fog = new THREE.FogExp2(0x111111, .13); // distance background color
+  var hackersWrapper = $('.hackers-wrapper');
+
+  var camera = new THREE.PerspectiveCamera(42, hackersWrapper.innerWidth() / hackersWrapper.innerHeight(), 0.1, 2000);
+
+  var renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
+
+  renderer.setClearColor(0x111111); // background color
+  renderer.setSize(hackersWrapper.innerWidth(), hackersWrapper.innerHeight());
+  $('.hackers-wrapper').append(renderer.domElement);
+
+  $(window).resize(onWindowResize);
+
+  function onWindowResize() {
+    camera.aspect = hackersWrapper.innerWidth() / hackersWrapper.innerHeight();
+    camera.updateProjectionMatrix();
+    renderer.setSize(hackersWrapper.innerWidth(), hackersWrapper.innerHeight());
+  }
+
+  //Floor
+  var floorG = new THREE.BoxGeometry(20, .10, 20);
+  var floorM = new THREE.MeshLambertMaterial({
+    color: 0x111111
+  });
+  var floor = new THREE.Mesh(floorG, floorM);
+  scene.add(floor);
+
+  //Buildings
+  var cube = [];
+  for (var i = 0; i < 800; ++i) {
+    var rHeight = (Math.random() * 5) + .25;
+    var geometry = new THREE.BoxGeometry(.25, rHeight, .25);
+    var material = new THREE.MeshLambertMaterial({
+      color: 0x65eee9
+    });
+    material.transparent = true;
+    material.opacity = .2;
+    cube[i] = new THREE.Mesh(geometry, material);
+    floor.add(cube[i]);
+
+    var x = (Math.random() * (10.00 - -10) + -10).toFixed(2);
+    var y = 0;
+    var z = (Math.random() * (10.00 - -10) + -10).toFixed(2);
+    cube[i].position.set(x, y, z);
+  }
+
+  //camera
+  camera.position.set(0, 2, 1);
+
+  //lights
+  var light1 = new THREE.DirectionalLight(0xffffff, 1);
+  scene.add(light1);
+  light1.position.set(1.5, 2, 1);
+
+  var light1 = new THREE.DirectionalLight(0xffffff, .5);
+  scene.add(light1);
+  light1.position.set(-1.5, 2, 1);
+
+
+  var distance = 0;
+  var floorRotation = 1;
+  var cameraPosition = 1;
+  var easingAmount = .001;
+
+  function render() {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+
+    //move camera and city to mouse movement slowly
+    var xDistance = floorRotation - floor.rotation.y;
+    var yDistance = cameraPosition - camera.position.z;
+    distance = Math.sqrt(xDistance * xDistance + yDistance * yDistance);
+    if (distance > 0) {
+      floor.rotation.y += xDistance * easingAmount;
+      camera.position.z += yDistance * easingAmount;
+    }
+  }
+  render();
+
+  //mouse movement
+
+  $('canvas').on('mousemove.control', function (e) {
+    var rotateDamper = 600;
+    var cameraDamper = 70;
+
+    floorRotation = -((e.clientX - $('canvas').width()) / rotateDamper);
+    cameraPosition = ((e.clientY) / cameraDamper);
+  });
+
+  // $(window).scroll(function () {
+  //   camera.position.y = 3 - ($(this).scrollTop() / 200);
+  // });
+
 
 
 
