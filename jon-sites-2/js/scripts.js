e8f4994ef813ea0f98f1c7ccad7f4f9ae67744a5
Josh Lopez <devjoshlopez@gmail.com>
2019-07-01 02:20:39 -0600

    revisions

--- jon-sites-2/js/scripts.js
+++ jon-sites-2/js/scripts.js
@@ -12,57 +12,63 @@ $(document).ready(function() {
     arrows: false
     // appendDots: document.getElementById("sliderDots")
   });
-  var progressBar = $(".progress");
-  var progressBarLabel = $(".slider__label");
+  var slideProgressBar = $(".progress.slide");
+  var slideProgressBarLabel = $(".slider__label");
+  var videoProgressBar = $(".progress.video");
+  var videoProgressBarLabel = $(".video__label");
+
+  document.getElementById("ekoVideo").contentWindow.postMessage("eko.pause", "*");
 
   slider.on("beforeChange", function(event, slick, currentSlide, nextSlide) {
-    var vid = $(slick.$slides[currentSlide]).find("video");
-    if (vid.length > 0) {
+    var currentVid = $(".slick-slide.slick-current").find("video")[0];
+    if (currentVid && !currentVid.hasAttribute("autoplay")) {
       slider.slick("slickPause");
-      vid.get(0).pause();
+      currentVid.pause();
+      currentVid.removeEventListener('timeupdate', videoProgressUpdate, false);
     }
 
-    var iframeVid = $(slick.$slides[currentSlide]).find("iframe");
-    if (iframeVid.length > 0) {
-      document
-        .getElementById("ekoVideo")
-        .contentWindow.postMessage("eko.pause", "*");
+    var iframeVid = $(".slick-slide.slick-current").find("iframe")[0];
+    if (iframeVid) {
+      document.getElementById("ekoVideo").contentWindow.postMessage("eko.pause", "*");
     }
 
     var calc = (nextSlide / (slick.slideCount - 1)) * 100;
-    progressBar
-      .css("background-size", calc + "% 100%")
-      .attr("aria-valuenow", calc);
+    slideProgressBar.css("background-size", calc + "% 100%").attr("aria-valuenow", calc);
+    slideProgressBarLabel.text(calc + "% completed");
 
-    progressBarLabel.text(calc + "% completed");
+    $(".button-group").hide(300);
   });
 
   slider.on("afterChange", function(event, slick, currentSlide) {
-    var vid = $(slick.$slides[currentSlide]).find("video");
-    if (vid.length > 0) {
+    var currentVid = $(".slick-slide.slick-current").find("video")[0];
+    if (currentVid) {
       slider.slick("slickPause");
-      $(vid)
-        .get(0)
-        .play();
-    }
-
-    var iframeVid = $(slick.$slides[currentSlide]).find("iframe");
-    if (iframeVid.length > 0) {
-      document
-        .getElementById("ekoVideo")
-        .contentWindow.postMessage("eko.play", "*");
-    }
-  });
-
-  $("video").on("ended", function() {
-    if (this.hasAttribute("autonext")) {
-      slider.slick("slickNext");
+      currentVid.pause();
+      currentVid.currentTime = 0;
+      currentVid.play();
+      videoProgressBar.show(300);
+      currentVid.addEventListener('timeupdate', videoProgressUpdate, false);
     } else {
-      // show buttons
-      $(".button-group").show(300);
+      videoProgressBar.hide(300);
     }
-    console.log("Video Complete");
+
+    var iframeVid = $(".slick-slide.slick-current").find("iframe")[0];
+    if (iframeVid) {
+      document.getElementById("ekoVideo").contentWindow.postMessage("eko.play", "*");
+    }
+
+    $(".slick-slide.slick-current video").on("ended", function() {
+      if (this.hasAttribute("autonext")) {
+        slider.slick("slickNext");
+      } else {
+        // show buttons
+        $(".button-group").show(300);
+      }
+      console.log("Video Complete");
+    });
   });
+
+
 
   $(".slick-dots li button").on("click", function(e) {
     e.stopPropagation();
@@ -73,7 +79,19 @@ $(document).ready(function() {
     $(".video-modal video")[0].pause();
   });
 
+  function videoProgressUpdate() {
+    var currentVid = $(".slick-slide.slick-current").find("video")[0];
+    const percent = (currentVid.currentTime / currentVid.duration) * 100;
+    videoProgressBar.css("background-size", percent + "% 100%").attr("aria-valuenow", percent);
+    videoProgressBarLabel.text(percent + "% completed");
+  }
+
 });
+
+
+function previousSlide() {
+  $(".slider-container").slick("slickPrev");
+}
 
 function nextSlide() {
   $(".slider-container").slick("slickNext");
@@ -90,10 +108,29 @@ function restartCurrentVideo() {
   }
 }
 
+function pauseCurrentVideo() {
+  var currentVid = $(".slick-slide.slick-current").find("video");
+
+  if (currentVid.length > 0) {
+    $(".slider-container").slick("slickPause");
+    if (currentVid.paused == false) {
+      currentVid.pause();
+    } else {
+      currentVid.play();
+    }
+  }
+}
+
 function showModalVideo(videoUrl) {
   console.log("show modal video" + videoUrl);
   $(".video-modal video source").attr( "src", videoUrl);
   $(".video-modal").addClass("showing");
   $(".video-modal video")[0].load();
   $(".video-modal video")[0].play();
+}
+
+function showOption(optionNumber) {
+  console.log("Show option " + optionNumber);
+  $(".slider-container").slick("slickNext");
+  
 }
